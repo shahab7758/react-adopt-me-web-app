1b23c9e8ed0019e0451e68e7c37bdc3817dee14d
waqas nazir <vickydev31113@gmail.com>
2020-12-25 17:24:41 +0500

    component rendered using api

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
+import pf from 'petfinder-client'
+import { navigate } from '@reach/router';
+
+const petfinder = pf({
+    key: process.env.API_KEY,
+    secret:process.env.API_SECRET
+})
 class Details extends Component {
-    state = {  }
+    constructor(props){
+        super(props)
+        this.state={
+            loading: true
+        }
+    }
+    componentDidMount() {
+        petfinder.pet.get({
+            output: 'full',
+            id: this.props.id
+        }).then(data =>{
+            const pet = data.petfinder.pet;
+            let breed;
+            if(Array.isArray(pet.breeds.breed)){
+                breed = pet.breeds.breed.join(', ')
+            }
+            else{
+                breed = pet.breeds.breed
+            }
+            this.setState({ 
+                name: pet.name,
+                animal:pet.animal,
+                location:`${pet.contact.city}, ${pet.contact.state}`,
+                description:pet.description,
+                media: pet.media,
+                breed,
+                loading:false
+              });
+        }).catch(()=>{
+            navigate('/')
+        })
+
+    }
     render() { 
-        return ( 
-            <div>
-            <h1>Hi lol</h1>
+        const {loading, name, animal, breed, location, description} = this.state
+        if(loading){
+            return ( 
+                <h1>Loading...</h1>
+             );
+        }else{
+        return(
+            <div className='details'>
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{location}</h2>
+                    <p>{description}</p>
+                </div>
             </div>
-         );
+        )}
+
     }
 }
  
--- src/Pet.js
+++ src/Pet.js
@@ -12,7 +12,7 @@ class Pet extends Component {
         return ( 
             <Link to={`/details/${id}`} className='pet'>
                 <div className='image-container'>
-                    <img src={photos[0].value} />
+                    <img src={photos[0].value} alt={name} />
                 </div>
                 <div className='info'>
                     <h1>{name}</h1>
