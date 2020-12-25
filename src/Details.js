6ab685421b217f4eb9994b816a888f10b69cf780
waqas nazir <vickydev31113@gmail.com>
2020-12-25 14:33:12 +0500

    refactored component for adding routing

---
+++ src/Details.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Details extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+            <h1>Hi lol</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Details;
